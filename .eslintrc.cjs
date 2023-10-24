54c5384f13e341c95a3e4ec4527a2d0adedd1399
Rafael Dametto <cacotpds@gmail.com>
2023-10-24 08:45:13 -0300

    Fix .eslintrc.cjs and .prettierrc files.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true, es2020: true, jest: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,20 @@
-{
-  "arrowParens": "always",
-    "bracketSpacing": true,
-      "jsxBracketSameLine": false,
-        "jsxSingleQuote": false,
-          "printWidth": 80,
-            "proseWrap": "always",
-              "quoteProps": "as-needed",
-                "semi": true,
-                  "singleQuote": true,
-                    "tabWidth": 2,
-                      "trailingComma": "all",
-                        "useTabs": false,
-                          "endOfLine": "auto"
-}
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: 'off',
+};
 
